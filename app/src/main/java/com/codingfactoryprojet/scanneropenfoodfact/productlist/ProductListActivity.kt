a226c38b4eb6288bc1ec51d0ceb1b279cce690f8
Basile Beyable <basile@beyable.com>
2021-03-10 16:00:58 +0100

    feat05 RecyclerView MVVM

---
+++ app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/productlist/ProductListActivity.kt
@@ -0,0 +1,41 @@
+package com.codingfactoryprojet.scanneropenfoodfact.productlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.codingfactoryprojet.scanneropenfoodfact.R
+import com.codingfactoryprojet.scanneropenfoodfact.databinding.ActivityProductListBinding
+
+class ProductListActivity : AppCompatActivity() {
+
+    private val model: ProductListViewModel by viewModels()
+
+    private lateinit var binding: ActivityProductListBinding
+    private lateinit var adapter: ProductAdapter
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProductListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //submit Live Data to get Products
+        model.getProductsLiveData().observe(this, Observer { products -> updateProducts(products!!)})
+
+
+        adapter = ProductAdapter(listOf())
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //call the ViewModel.loadProducts function
+        model.loadProducts()
+    }
+
+    private fun updateProducts(products: List<Product>) {
+        adapter.updateDataSet(products)
+    }
+}
