5024cf391d5fe7a860cc620483f969800ce7ee45
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2021-03-10 14:45:44 +0100

    feat: implementing product models and api request

--- app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/MainActivity.kt
+++ app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/MainActivity.kt
@@ -2,10 +2,12 @@ package com.codingfactoryprojet.scanneropenfoodfact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codingfactoryprojet.scanneropenfoodfact.product.getProductData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getProductData(737628064502)
     }
 }
