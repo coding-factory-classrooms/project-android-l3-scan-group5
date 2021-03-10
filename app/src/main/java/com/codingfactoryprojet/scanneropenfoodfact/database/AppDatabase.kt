37f6ceee4b629b5ff28fd6a81c391bee87f990cd
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2021-03-10 15:45:44 +0100

    feat: implement singleton of appDatabase

--- app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/database/AppDatabase.kt
+++ app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.codingfactoryprojet.scanneropenfoodfact.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.codingfactoryprojet.scanneropenfoodfact.entity.product.Product
 import com.codingfactoryprojet.scanneropenfoodfact.service.product.ProductDao
@@ -8,4 +10,21 @@ import com.codingfactoryprojet.scanneropenfoodfact.service.product.ProductDao
 @Database(entities = [Product::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
