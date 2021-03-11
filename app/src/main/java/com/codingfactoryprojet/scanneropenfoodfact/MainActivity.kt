d2a0ea110f3c405abfd2f78196dc719efaa15033
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2021-03-11 18:21:57 +0100

    feat: remove unecessary func call and update android manifest

--- app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/MainActivity.kt
+++ app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/MainActivity.kt
@@ -2,14 +2,10 @@ package com.codingfactoryprojet.scanneropenfoodfact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.codingfactoryprojet.scanneropenfoodfact.entity.product.getProductData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getProductData(
-            737628064502
-        )
     }
 }
