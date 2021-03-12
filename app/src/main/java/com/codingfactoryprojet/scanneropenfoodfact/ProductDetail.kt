0cdbb3e177184137540aaaeb5bf3e75d971ebbf4
Lunahey <55185494+Lunahey@users.noreply.github.com>
2021-03-12 12:55:15 +0100

    Ajout couleur

--- app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/ProductDetail.kt
+++ app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/ProductDetail.kt
@@ -1,5 +1,6 @@
 package com.codingfactoryprojet.scanneropenfoodfact
 
+import android.graphics.Color
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,14 +20,33 @@ class ProductDetail : AppCompatActivity() {
         val labels = intent.getStringExtra("labels")
         val category = intent.getStringExtra("categories")
         val calories = intent.getIntExtra("calories100", 0)
-        val nutriscore = intent.getStringExtra("nutriscore")
+        var nutriscore = intent.getStringExtra("nutriscore")
         val urlImage = intent.getStringExtra("urlImage")
 
         binding.valueCategorie.text = category
         binding.valueLabel.text = labels
         binding.valueCalories.text = calories.toString()
         binding.valueName.text = name
-        binding.valueNutriscore.text = nutriscore.toUpperCase()
+
         Picasso.get().load(urlImage).into(binding.ProductimageView)
+        binding.valueNutriscore.text = nutriscore.toUpperCase()
+
+        if(nutriscore.toString().equals("e")){
+            binding.valueNutriscore.setTextColor(Color.RED)
+        }
+        else if(nutriscore.toString().equals("a")){
+            binding.valueNutriscore.setTextColor(Color.GREEN)
+        }
+        else if (nutriscore.toString().equals("d")){
+            binding.valueNutriscore.setTextColor(Color.MAGENTA)
+        }
+        else if(nutriscore.toString().equals("b")){
+            binding.valueNutriscore.setTextColor(Color.BLUE)
+
+        }
+        else if (nutriscore.toString().equals("c")){
+            binding.valueNutriscore.setTextColor(Color.CYAN)
+
+        }
     }
 }
