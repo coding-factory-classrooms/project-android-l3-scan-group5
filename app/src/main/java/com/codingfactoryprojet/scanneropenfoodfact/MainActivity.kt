4e585f58699c4dcc072094a4d67f4a7bf6a76fa4
Jacky LIM <jacky@MacBook-Pro-de-Jacky.local>
2021-03-12 11:06:19 +0100

    feat: navigation

--- app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/MainActivity.kt
+++ app/src/main/java/com/codingfactoryprojet/scanneropenfoodfact/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.codingfactoryprojet.scanneropenfoodfact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codingfactoryprojet.scanneropenfoodfact.databinding.ActivityMainBinding
+import com.codingfactoryprojet.scanneropenfoodfact.productlist.ProductListActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.bottomNavigation.selectedItemId = R.id.Home
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            item ->
+            when (item.itemId) {
+                R.id.Home -> {
+                    val intent = Intent(this, ProductListActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            true
+        }
     }
 }
